64f29a6ade406cc2f1d5071ac7eb97c939a15367
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-18 21:05:04 +0200

    Working on Gradle Errors that are occuring

--- app/src/main/java/com/example/teachandlearn/Form1Activity.java
+++ app/src/main/java/com/example/teachandlearn/Form1Activity.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
