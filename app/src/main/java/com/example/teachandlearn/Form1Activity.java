4955dc5e83ef6140b2e0f5beb7f8ba95de83327d
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-18 20:24:46 +0200

    Making the Sign Up Option Activity JAVA Class

--- app/src/main/java/com/example/teachandlearn/Form1Activity.java
+++ app/src/main/java/com/example/teachandlearn/Form1Activity.java
@@ -1,9 +1,11 @@
+
+package com.example.teachandlearn;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
