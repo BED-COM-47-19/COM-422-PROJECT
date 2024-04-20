67906bed7784d7d1d62521e3ac5b8ac8ed811fdc
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-20 20:39:20 +0200

    TeacherUploads changing the functionalities of methods

--- app/src/main/java/com/example/teachandlearn/TeacherUploadsActivity.java
+++ app/src/main/java/com/example/teachandlearn/TeacherUploadsActivity.java
@@ -1,13 +1,9 @@
-//
-//
+
 package com.example.teachandlearn;
 import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 
