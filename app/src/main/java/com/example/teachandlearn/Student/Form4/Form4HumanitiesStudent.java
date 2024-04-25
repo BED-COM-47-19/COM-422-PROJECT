cd9bb8af9acc1b69f47730d80ec47d13f4d17ae7
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-25 07:56:46 +0200

    Setting up and checking if the Buttons in Form 4 Class are working

--- app/src/main/java/com/example/teachandlearn/Student/Form4/Form4HumanitiesStudent.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form4/Form4HumanitiesStudent.java
@@ -1,12 +1,11 @@
-package com.example.teachandlearn.Student.Form4;
 
+package com.example.teachandlearn.Student.Form4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.teachandlearn.R;
 
 public class Form4HumanitiesStudent extends AppCompatActivity {
--- app/src/main/java/com/example/teachandlearn/Student/Form4/Form4StudentViewContent.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form4/Form4StudentViewContent.java
@@ -1,11 +1,10 @@
-package com.example.teachandlearn.Student.Form4;
 
+package com.example.teachandlearn.Student.Form4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.teachandlearn.R;
 import com.example.teachandlearn.Teacher.Uploads.TeacherUploads;
 
