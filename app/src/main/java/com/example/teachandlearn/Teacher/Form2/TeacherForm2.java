50844a4f5a0882f5087646d5a1f94ba90b9ac112
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-25 08:00:32 +0200

    Making conections between Upload Class and Form 2 Teacher Class

--- app/src/main/java/com/example/teachandlearn/Teacher/Form2/TeacherForm2.java
+++ app/src/main/java/com/example/teachandlearn/Teacher/Form2/TeacherForm2.java
@@ -1,13 +1,11 @@
 
 package com.example.teachandlearn.Teacher.Form2;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.teachandlearn.R;
 
 public class TeacherForm2 extends AppCompatActivity {
