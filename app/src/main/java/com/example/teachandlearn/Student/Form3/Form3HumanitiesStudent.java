a01077767a501c53db7dfc5bf7fb82e33dd28a9f
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-25 07:55:29 +0200

    Setting up and checking if the Buttons in Form 3 Class are working

--- app/src/main/java/com/example/teachandlearn/Student/Form3/Form3HumanitiesStudent.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form3/Form3HumanitiesStudent.java
@@ -1,12 +1,11 @@
-package com.example.teachandlearn.Student.Form3;
 
+package com.example.teachandlearn.Student.Form3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.teachandlearn.R;
 
 public class Form3HumanitiesStudent extends AppCompatActivity {
--- app/src/main/java/com/example/teachandlearn/Student/Form3/Form3StudentViewContent.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form3/Form3StudentViewContent.java
@@ -1,11 +1,10 @@
-package com.example.teachandlearn.Student.Form3;
 
+package com.example.teachandlearn.Student.Form3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.teachandlearn.R;
 import com.example.teachandlearn.Teacher.Uploads.TeacherUploads;
 
