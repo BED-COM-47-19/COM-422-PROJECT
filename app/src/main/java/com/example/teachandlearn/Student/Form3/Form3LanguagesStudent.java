773d861aaa2075aac70e05b4b9ea695b97696c7e
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-28 14:28:53 +0200

    Changing the Design the Back Button for Student Form 3 Languages Class

--- app/src/main/java/com/example/teachandlearn/Student/Form3/Form3LanguagesStudent.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form3/Form3LanguagesStudent.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.teachandlearn.R;
 
 public class Form3LanguagesStudent extends AppCompatActivity {
 
+    private ImageButton buttonBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,7 @@ public class Form3LanguagesStudent extends AppCompatActivity {
         // Find buttons for each humanities subject
         Button buttonEnglish = findViewById(R.id.buttonEnglish);
         Button buttonChichewa = findViewById(R.id.buttonChichewa);
+        buttonBack = findViewById(R.id.button_back);
 
 
         // Set click listeners for each button
@@ -37,6 +40,13 @@ public class Form3LanguagesStudent extends AppCompatActivity {
             }
         });
 
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Logic for when the back button is pressed
+                onBackPressed();
+            }
+        });
 
     }
 
@@ -50,4 +60,12 @@ public class Form3LanguagesStudent extends AppCompatActivity {
         Intent intent = new Intent(Form3LanguagesStudent.this, Form3StudentViewContent.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed() {
+        // Handle the back button action
+        super.onBackPressed();
+        // You can also add custom logic here if needed
+    }
+
 }
