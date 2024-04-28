a7189200031f745891f9e7d6f33219a2630e3113
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-04-28 19:01:26 +0200

    Working with Student Form 3 View Content Classes XML

--- app/src/main/java/com/example/teachandlearn/Student/Form3/Form3StudentViewContent.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form3/Form3StudentViewContent.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.teachandlearn.R;
 import com.example.teachandlearn.Teacher.Form3.TeacherForm3Uploads;
@@ -11,6 +13,7 @@ import com.example.teachandlearn.Teacher.Form3.TeacherForm3Uploads;
 
 public class Form3StudentViewContent extends AppCompatActivity {
 
+    private ImageButton buttonBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,7 @@ public class Form3StudentViewContent extends AppCompatActivity {
         Button buttonAudio = findViewById(R.id.button_audio);
         Button buttonVideos = findViewById(R.id.button_videos);
         Button buttonQuestions = findViewById(R.id.button_tests_quizzes);
+        buttonBack = findViewById(R.id.button_back);
 
         // Set click listeners for each button
         buttonPDF.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +54,15 @@ public class Form3StudentViewContent extends AppCompatActivity {
                 navigateToTeacherUploads("question");
             }
         });
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Logic for when the back button is pressed
+                onBackPressed();
+            }
+        });
+
     }
 
     // Helper method to navigate to TeacherUploadsActivity
@@ -58,6 +71,13 @@ public class Form3StudentViewContent extends AppCompatActivity {
         intent.putExtra("content_type", contentType);
         intent.putExtra("action", "view");  // This specifies that the student wants to view content
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Handle the back button action
+        super.onBackPressed();
+        // You can also add custom logic here if needed
     }
 
 }
