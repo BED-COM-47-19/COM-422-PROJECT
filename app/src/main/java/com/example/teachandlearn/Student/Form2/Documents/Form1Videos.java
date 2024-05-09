a020ea9d35743a2f06e1fe96288e68c60c631afc
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-05-09 22:18:36 +0200

    Making changes in the FORM 1 Document XML Files

---
+++ app/src/main/java/com/example/teachandlearn/Student/Form2/Documents/Form1Videos.java
@@ -0,0 +1,36 @@
+
+
+package com.example.teachandlearn.Student.Form1.Documents;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.example.teachandlearn.Student.Form1.Documents.VideoAdapter;
+import com.example.teachandlearn.R;
+
+
+public class Form1Videos extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private VideoAdapter videoAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form1_video);
+
+        recyclerView = findViewById(R.id.rvVideos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize your Firebase components and setup your query
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("videos");
+        Query query = databaseReference.orderByKey();
+
+        videoAdapter = new VideoAdapter(query, this); // You'll need to create a custom adapter
+        recyclerView.setAdapter(videoAdapter);
+    }
+}
+
