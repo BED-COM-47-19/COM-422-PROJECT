43b0718b453f7568e4b362a79e83788e4583affb
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-05-14 18:29:13 +0200

    Resolving Gradle Errors

--- app/src/main/java/com/example/teachandlearn/Student/LogIn_And_SignUp/StudentSignUp.java
+++ app/src/main/java/com/example/teachandlearn/Student/LogIn_And_SignUp/StudentSignUp.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.teachandlearn.R;
 import com.example.teachandlearn.Student.SelectClass.StudentSelectClass;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+
 
 public class StudentSignUp extends AppCompatActivity {
 
