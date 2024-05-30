c1629fce99ed6cf65ec8657a85c41496b3baa2c4
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-05-30 23:50:28 +0200

    Adding other Actvity in Manifest XML

--- app/src/main/java/com/example/teachandlearn/Student/Form1/Documents/Form1PDF.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form1/Documents/Form1PDF.java
@@ -54,7 +54,7 @@ public class Form1PDF extends AppCompatActivity {
         fetchPDFsFromFirebase();
 
         studentEmail = getIntent().getStringExtra("student_form1_emails");
-        
+
         if (studentEmail != null) {
             // Store student email to Firebase when the intent has student email
             saveStudentEmailToFirebase(studentEmail);
