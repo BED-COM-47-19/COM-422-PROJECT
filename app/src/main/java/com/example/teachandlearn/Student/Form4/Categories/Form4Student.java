e9195ac704bfd52f3229b12008d54b546e9d59fe
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-05-31 00:10:40 +0200

    Still Modfying the Student Form 4 Java Class

--- app/src/main/java/com/example/teachandlearn/Student/Form4/Categories/Form4Student.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form4/Categories/Form4Student.java
@@ -1,5 +1,6 @@
-package com.example.teachandlearn.Student.Form4.Categories;
 
+
+package com.example.teachandlearn.Student.Form4.Categories;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
