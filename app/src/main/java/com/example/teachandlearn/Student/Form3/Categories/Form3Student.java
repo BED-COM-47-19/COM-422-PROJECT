c96f0016a262aac86bf8fc603979f98450a0a824
BED-COM-47-19 <bed-com-47-19@unima.ac.mw>
2024-05-31 00:08:33 +0200

    Modfying the Student Form 3 Java Class

--- app/src/main/java/com/example/teachandlearn/Student/Form3/Categories/Form3Student.java
+++ app/src/main/java/com/example/teachandlearn/Student/Form3/Categories/Form3Student.java
@@ -1,5 +1,5 @@
-package com.example.teachandlearn.Student.Form3.Categories;
 
+package com.example.teachandlearn.Student.Form3.Categories;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.teachandlearn.R;
+
 
 public class Form3Student extends AppCompatActivity {
 
